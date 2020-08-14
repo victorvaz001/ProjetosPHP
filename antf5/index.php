d35da5cf142943d93441478564c2ed70f8727b58
Victor Gonçalves <victorvaz001@gmail.com>
2020-08-14 16:41:07 -0300

    Projeto ant F5

---
+++ antf5/index.php
@@ -0,0 +1,16 @@
+<?php
+	#############################################################
+	/*Projeto anti F5 + geração de arquivo txt*/
+	if(isset($_POST['nome']))
+	{
+		$nome = $_POST['nome'];
+		file_put_contents("teste.txt", $nome, FILE_APPEND);
+
+		header("Location: index.php"); //não envia o formulairo caso eu aprte F5
+	}
+?>
+
+	<form method="POST">
+		<input type="text" name="nome">
+		<input type="submit" value="Enviar">
+	</form>
