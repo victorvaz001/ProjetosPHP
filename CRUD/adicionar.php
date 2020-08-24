6dc078f75b8af3fd70cf5536da620217c69b679a
Victor Gonçalves <victorvaz001@gmail.com>
2020-08-24 17:22:43 -0300

    Adição do footer no adicionar.php

--- CRUD/adicionar.php
+++ CRUD/adicionar.php
@@ -45,8 +45,28 @@ if(isset($_POST["nome"]) && !empty($_POST["nome"])){
       <input type="password" class="form-control" name="senha" required
       placeholder="Digite sua senha">
     </div>
-    <button type="submit" class="btn btn-success">Save</button>
+    <button type="submit" class="btn btn-success">Save</button><br/><br/>
   </form>
+  <div class="card text-center">
+  <div class="card-header">
+    Sistema de Controle de Usuários
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Tecnologias</h5>
+    <p class="card-text">PHP 7.4.8 - PDO - MySQL - Bootstrap </p>
+     <h5 class="card-title">Programador</h5>
+     <p class="card-text">Victor Gonçalves Vaz<br>
+       victorvaz001@gmail.com</p>
+    <a href="index.php" class="btn btn-primary">Home</a>
+  </div>
+  <div class="card-footer text-muted">
+    <?php
+    date_default_timezone_set('America/Sao_Paulo');
+  $dataAtual = date("d/m/yy \-\ H:i:s");
+  echo "Todos os direitos reservados &copy; - ".$dataAtual;
+    ?>
+  </div>
+</div>
 </div>
 </body>
 </html>
