c44518081edbc4b775182ca35f281fcd2443b617
Victor Gonçalves <victorvaz001@gmail.com>
2020-08-27 11:55:58 -0300

    Projeto para Ordenar Resultados

---
+++ Ordenação-de-Resultados/README.md
@@ -0,0 +1,15 @@
+# Projeto - Ordenação de resultados<br>
+
+## Objetivo
+* Projeto criado para ordernar de resultados, por idade e nome<br>
+  
+## Tecnologias
+
+* PHP Version 7.4.8<br>
+* MySQL
+* HTML<br>
+
+
+## Testes<br>
+
+* Selecionar um campo no select para ordenação
---
+++ Ordenação-de-Resultados/index.php
@@ -0,0 +1,52 @@
+<?php
+	try{
+		$pdo = new PDO("mysql:dbname=projeto_ordenar;host=localhost", "root", "root");
+	}catch(PDOException $e){
+		echo "ERRO: ".$e->getMessage();
+		exit;
+	}
+if(isset($_GET['ordem']) && !empty($_GET['ordem'])){
+		$ordem = addslashes($_GET['ordem']);
+		$sql = "SELECT * FROM usuarios ORDER BY ".$ordem;
+	} else {
+		$ordem = '';
+		$sql = "SELECT * FROM usuarios";
+	}
+?>
+
+<form method="GET">
+	<select name="ordem" onchange="this.form.submit()"><!--Envia o formulário-->
+		<option></option>
+		<option value="nome"<?php echo ($ordem=="nome")?'selected="selected"':''; ?>>Pelo nome</option>
+		<option value="idade" <?php echo ($ordem=="idade")?'selected="selected"':''; ?>>Pela idade</option>
+	</select>
+</form>
+
+<table border="1" width="400px">
+
+	<tr>
+		<th>Nome</th>
+		<th>Idade</th>
+	</tr>
+
+	<?php
+
+
+	$sql = $pdo->query($sql);
+
+	if($sql->rowCount() > 0){
+		foreach($sql->fetchAll() as $usuario):
+	?>
+
+			<tr>
+				<td><?php echo $usuario['nome']; ?></td>
+				<td><?php echo $usuario['idade']; ?></td>
+			</tr>
+		<?php
+		endforeach;
+
+		
+	}
+	?>
+	
+</table>
