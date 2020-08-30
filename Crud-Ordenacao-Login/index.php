528497edf692d3d9fa45abcaaa43d8e018335320
Victor Gonçalves <victorvaz001@gmail.com>
2020-08-30 00:05:47 -0300

    ident

--- Crud-Ordenacao-Login/index.php
+++ Crud-Ordenacao-Login/index.php
@@ -4,13 +4,12 @@ require 'config.php';
 
 if(isset($_GET["ordem"]) && !empty($_GET["ordem"])){
 
-      $ordem = addslashes($_GET["ordem"]);
-      $sql = "SELECT * FROM aluno ORDER BY ".$ordem;
+  $ordem = addslashes($_GET["ordem"]);
+  $sql = "SELECT * FROM aluno ORDER BY ".$ordem;
 } else {
-      $ordem = "";
-      $sql = "SELECT * FROM aluno";
+  $ordem = "";
+  $sql = "SELECT * FROM aluno";
 }
-
 
 ?>
 <!DOCTYPE html>
@@ -20,40 +19,40 @@ if(isset($_GET["ordem"]) && !empty($_GET["ordem"])){
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body style="background-color: #f5f5f5;">
-	 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm sticky-top">
-	<h5 class="my-0 mr-md-auto font-weight-normal"><a href="index.php"
-		style="text-decoration: none;">CRUD</h5></a>
- 
-  <nav class="my-2 my-md-0 mr-md-3">
-    <a class="p-2 text-dark" href="index.php">Home</a>
-    <a class="p-2 text-dark" href="adduser.php">New User</a>
-    <?php
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm sticky-top">
+   <h5 class="my-0 mr-md-auto font-weight-normal"><a href="index.php"
+    style="text-decoration: none;">CRUD</h5></a>
+
+    <nav class="my-2 my-md-0 mr-md-3">
+      <a class="p-2 text-dark" href="index.php">Home</a>
+      <a class="p-2 text-dark" href="adduser.php">New User</a>
+      <?php
       if(isset($_SESSION["id"]) && !empty($_SESSION["id"])){
 
-          $nome = $_SESSION["nome"];
+        $nome = $_SESSION["nome"];
 
-          echo '<a href="index.php"> Hello '.$nome.'</a>';
+        echo '<a href="index.php"> Hello '.$nome.'</a>';
       } else {
         header("Location: login.php");
       }
-    ?>
-     
-
-  </nav>
- <a class="btn btn-outline-primary" data-toggle="modal" data-target="#staticBackdrop"href="logout.php" onclick="return confirm('Finalizar sessão?');">Logout</a>
+      ?>
 
 
+    </nav>
+    <a class="btn btn-outline-primary" data-toggle="modal" data-target="#staticBackdrop"href="logout.php" onclick="return confirm('Finalizar sessão?');">Logout</a>
 
-</div>
 
 
-<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-  <h2 class="display-5">PHP - MySQL - Bootstrap</h2>
+  </div>
+
+
+  <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <h2 class="display-5">PHP - MySQL - Bootstrap</h2>
     <p class="lead">  
-   </p>
-</div>
- 
-<div class="container">
+    </p>
+  </div>
+
+  <div class="container">
 
 
 
@@ -69,46 +68,46 @@ if(isset($_GET["ordem"]) && !empty($_GET["ordem"])){
       <option value="sexo" <?php echo ($ordem=="sexo")?'selected="selected"':''?>>For Sex</option>
     </select>
 
-   </form>
-<br/><br/>
+  </form>
+  <br/><br/>
 
- <table class="table" style="text-align: center;">
+  <table class="table" style="text-align: center;">
 
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Name</th>
-      <th scope="col">E-mail</th>
-      <th scope="col">Age</th>
-      <th scope="col">Sex</th>
-      <th scope="col" colspan="2">Actions</th>
-    </tr>
-  </thead>
-  <?php
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Name</th>
+        <th scope="col">E-mail</th>
+        <th scope="col">Age</th>
+        <th scope="col">Sex</th>
+        <th scope="col" colspan="2">Actions</th>
+      </tr>
+    </thead>
+    <?php
     
-      $sql = $pdo->query($sql);
+    $sql = $pdo->query($sql);
 
     if($sql->rowCount()> 0){
       foreach($sql->fetchAll() as $usuario):
-  ?>
-  <tbody>
-    <tr>
-      <th scope="row"><?php echo $usuario["id"]; ?></th>
-      <td><?php echo $usuario["nome"]; ?></td>
-      <td><?php echo $usuario["email"]; ?></td>
-      <td><?php echo $usuario["idade"]; ?></td>
-      <td><?php echo $usuario["sexo"]; ?></td>
-      <td><?php echo '<a href="updateuser.php?id='.$usuario["id"].'" class="btn btn-outline-info">Update</a>'?></td>
+        ?>
+        <tbody>
+          <tr>
+            <th scope="row"><?php echo $usuario["id"]; ?></th>
+            <td><?php echo $usuario["nome"]; ?></td>
+            <td><?php echo $usuario["email"]; ?></td>
+            <td><?php echo $usuario["idade"]; ?></td>
+            <td><?php echo $usuario["sexo"]; ?></td>
+            <td><?php echo '<a href="updateuser.php?id='.$usuario["id"].'" class="btn btn-outline-info">Update</a>'?></td>
 
-      <td onclick="return confirm('Confirmar Exclusão?');"><?php echo '<a href="deleteuser.php?id='.$usuario["id"].'" class="btn btn-outline-danger">Delete</a>'?></td>
-    </tr>
-  </tbody>
-  <?php
-    endforeach;
-  }
-  ?>
-</table>
-<footer class="pt-4 my-md-5 pt-md-5 border-top">
+            <td onclick="return confirm('Confirmar Exclusão?');"><?php echo '<a href="deleteuser.php?id='.$usuario["id"].'" class="btn btn-outline-danger">Delete</a>'?></td>
+          </tr>
+        </tbody>
+        <?php
+      endforeach;
+    }
+    ?>
+  </table>
+  <footer class="pt-4 my-md-5 pt-md-5 border-top">
     <div class="row">
       <div class="col-6 col-md">
       	<h5>Meu Github</h5>
